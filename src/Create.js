d84933ed36bf40154d686a0f87e5b77802f1cecc
Ricky <ahujashaurya13@gmail.com>
2023-01-12 21:31:43 -0500

    changes to add new blog to the site

--- src/Create.js
+++ src/Create.js
@@ -1,25 +1,61 @@
-const Create = () => {
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
-    
+const Create = () => {
+    const [title, setTitle] = useState('enter value');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('mario');
+
+    const [isPending, setIsPending] = useState(false);
+    const history = useHistory();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const blog = {title, body, author};
+
+        setIsPending(true)
+
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(blog)
+        }).then(() => {
+            console.log('New blog added')
+            setIsPending(false)
+            // history.go(-1)
+            history.push('/')
+            })
+    }
     return (
         <div className="create">
             <h2>Add a New Blog</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Blog title:</label>
                 <input 
                     type="text"
                     required
+                    // value = {title}
+                    onChange = {(e) => setTitle(e.target.value)}
                 />
                 <label>Blog body:</label>
                 <textarea
                     required
+                    value = {body}
+                    onChange = {(e) => setBody(e.target.value)}
                 ></textarea>
                 <label>Blog author:</label>
-                <select>
+                <select
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}>
                     <option value="mario">mario</option>
                     <option value="yoshi">yoshi</option>
                 </select>
-                <button>Add Blog</button>
+                {!isPending && <button>Add Blog</button>}
+                {isPending && <button disabled>Processing ...</button>}
+
+                {/* <p>{title}</p>
+                <p>{body}</p>
+                <p>{author}</p> */}
             </form>
         </div>
     );
